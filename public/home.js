ceb0060bc6c41d6e23ea776a398acbd6d5be4d8e
gitmlody <michal.kubacki7siwy2@gmail.com>
2020-05-14 19:01:10 +0200

    Second commit

--- public/home.js
+++ public/home.js
@@ -1,4 +1,4 @@
 const element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World2!"));
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World3!"));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 const element = (
     <div title="Outer div">
-      <h1>Hello World2!</h1>
+      <h1>Hello World3!</h1>
     </div>
   );
   
-  ReactDOM.render(element, document.getElementById('contents'));
+ReactDOM.render(element, document.getElementById('contents'));
